b9914eb60715207ec43ea338f61c9ddc3b549f30
AndresMeraz <andres.meraz312@gmail.com>
2023-11-14 17:18:44 -0800

    state and type functionality

--- Capstone Template/scripts/app.js
+++ Capstone Template/scripts/app.js
@@ -5,7 +5,7 @@ window.onload = () => {
   const states = document.getElementById("states");
   const parktype = document.getElementById("parktype");
   addLocationsToDropdown();
-  addNationalParksToDropdown();
+  addParkTypeToDropdown();
 };
 
 function addLocationsToDropdown() {
@@ -25,40 +25,89 @@ function addLocationsToDropdown() {
   });
 }
 
-function addNationalParksToDropdown() {
+function addParkTypeToDropdown() {
   // Get the dropdown element by its ID
-
-  // Use forEach to iterate over the array
-  nationalParksArray.forEach(function (park) {
-    // Create a new option element
-    let option = document.createElement("option");
-
-    // Set the value and text content for the new option
-    option.value = park.LocationName;
-    option.textContent = park.LocationName;
-
-    // Add the new option to the dropdown
-    parktype.appendChild(option);
-  });
+const parkType = document.getElementById("parktype");  //grabs value from ID
+for (let i = 0; i < parkTypesArray.length; i++) {   //for loop cycles through the array
+  const parkTypeOptions = new Option (parkTypesArray[i], parkTypesArray[i]);   // creates a new option in the dropdown for each array property
+  parkType.appendChild(parkTypeOptions); //adding each option to the dropdown
 }
-// grab the value of a selected item from the dropdown.
-function searchLocation() {
-  const selectedValue = states.value;
-  alert(selectedValue);
 }
-
 // Declaring variables
 
 
-//displaying the cards 
-function parkTemplate(park) {
-  return `
-        <div class="card" style="width: 18rem;">
-        <img src="${park.Image}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${park.LocationName}</h5>
-            <p class="card-text">${park.State}.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
+
+//grab the value from the state dropdown and match it with the value from the array
+
+function grabStateFromArray() {
+  let stateName = document.getElementById("states").value; 
+  let stateInfo = nationalParksArray.filter(state => state.State == stateName)
+  let message = ""
+  for (let i = 0; i < stateInfo.length; i++) {
+    message += 
+    `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+              <h5 class="card-title">${stateInfo[i].LocationName}</h5>
+              <p class="card-text">${stateInfo[i].City}, ${stateInfo[i].State}.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
         </div>`;
+  }
+  //displays the cards
+  document.getElementById("myParks").innerHTML = message
+}
+
+function grabTypeFromArray() {
+  let parkType = document.getElementById("parktype").value;
+  let typeInfo = nationalParksArray.filter(type => type.LocationName.includes(parkType))
+  let message = ""
+  for (let i = 0; i < typeInfo.length; i++) {
+    message += 
+    `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+              <h5 class="card-title">${typeInfo[i].LocationName}</h5>
+              <p class="card-text">${typeInfo[i].City}, ${typeInfo[i].State}.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>`;
+}
+document.getElementById("myParks").innerHTML = message
+}
+
+
+function grabBothFromArray() {
+  let stateName = document.getElementById("states").value;
+  let parkType = document.getElementById("parktype").value;
+  let bothInfo = nationalParksArray.filter(both => both.LocationName.includes(parkType) && both.State == stateName);
+  let message = ""
+  for (let i = 0; i < bothInfo.length; i++) {
+    message += 
+    `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+              <h5 class="card-title">${bothInfo[i].LocationName}</h5>
+              <p class="card-text">${bothInfo[i].City}, ${bothInfo[i].State}.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>`;
+}
+document.getElementById("myParks").innerHTML = message
+}
+
+
+function chooseStateOrType() {
+  let stateName = document.getElementById("states").value;
+  let parkType = document.getElementById("parktype").value;
+  console.log(typeof parkType);
+   if (stateName && parkType == "null") {
+    grabStateFromArray()
+  }
+  else if (parkType && stateName == "null") {
+    grabTypeFromArray()
+  }
+  else if (stateName && parkType) {
+    grabBothFromArray()
+  }
+  else {
+    alert("please choose")
+  }
 }
