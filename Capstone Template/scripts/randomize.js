e448bfa17443198dd77e9ea787f4eb156b8f19e5
AndresMeraz <andres.meraz312@gmail.com>
2023-11-16 16:41:38 -0800

    Finished MVP

--- Capstone Template/scripts/randomize.js
+++ Capstone Template/scripts/randomize.js
@@ -19,27 +19,31 @@ return `<div class="card" >
     </div>
     </div>`;
 }
-    // <img class="mountain-photo" src="images/${img}">
+
+function parkCard(name, address) {
+    return `<div class="card">
+    <div class="card-body">
+              <h5 class="card-title">${name}</h5><br>
+              <p class="card-text">Address: ${address}.</p>              
+          </div>
+        </div>`;
+}
     
     let getRandomData = () => {
         let cards = '';
         for (let i = 0; i < 6; i++) {
-            let randomMountainIndex = randomMountainIndexMaker();
-            cards+= mountainCard(mountainsArray[randomMountainIndex].name,mountainsArray[randomMountainIndex].desc);
+            let randomNumber = Math.random();
+            if(randomNumber < 0.5){
+                let randomMountainIndex = randomMountainIndexMaker();
+                cards += mountainCard(mountainsArray[randomMountainIndex].name,mountainsArray[randomMountainIndex].desc);
+
+            }else{
+            let randomParkIndex = randomParkIndexMaker();
+            cards += parkCard(nationalParksArray[randomParkIndex].LocationName, nationalParksArray[randomParkIndex].Address)
+            }
         }
         console.log(cards);
   document.getElementById('randomcard').innerHTML = cards;
 };
 
 window.onload = getRandomData;
-
-// window.addEventListener("load", getRandomUserData);
-// getUserBtn.addEventListener("click", getRandomUserData);
-
-//   imgContainer.innerHTML = `<img src=${mtnData.img}>`;
-//   details.innerHTML = `
-//       <h2>${parkData.LocationName}</h2>
-//       <h3>${mtnData.name}</h3>`;
-//   let randomColor =
-//     "#" + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0");
-//   document.documentElement.style.setProperty("--theme-color", randomColor);
